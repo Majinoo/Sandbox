24c74481773ca28a9590412b310aa55277cd5470
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-11-15 15:30:12 +0100

    Add project files.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Sandbox
+{
+    public class Monster
+    {
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seahorse seahorse = new Seahorse();
+            Shell shell = new Shell();
+            Monster monster = new Monster();
+            Icecream icecream = new Icecream();
+            Crab crab = new Crab();
+            Cake cake = new Cake();
+        }
+    }
+}
